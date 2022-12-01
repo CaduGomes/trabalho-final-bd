c794a7d03e2d629d4af28338455430bd47fc59a4
Matheus Jacks <matheusrjacks@gmail.com>
2022-12-01 18:03:38 -0300

    refactor: update DDL script

--- brModelo/DDL.sql
+++ brModelo/DDL.sql
@@ -1,18 +1,13 @@
-CREATE TABLE Brand (
-    id_brand varchar(255) PRIMARY KEY,
-    name varchar(255)
-);
-
-CREATE TABLE POI (
-    id_poi varchar(255) PRIMARY KEY,
-    id_address varchar(255),
+CREATE TABLE State (
+    id_state varchar(255) PRIMARY KEY,
     name varchar(255)
 );
 
 CREATE TABLE City (
     id_city varchar(255) PRIMARY KEY,
     id_state varchar(255),
-    name varchar(255)
+    name varchar(255),
+    FOREIGN KEY(id_state) REFERENCES State (id_state)
 );
 
 CREATE TABLE Address (
@@ -24,6 +19,18 @@ CREATE TABLE Address (
     number varchar(255),
     complement varchar(255),
     FOREIGN KEY(id_city) REFERENCES City (id_city)
+);
+
+CREATE TABLE POI (
+    id_poi varchar(255) PRIMARY KEY,
+    id_address varchar(255),
+    name varchar(255),
+    FOREIGN KEY(id_address) REFERENCES Address (id_address)
+);
+
+CREATE TABLE Brand (
+    id_brand varchar(255) PRIMARY KEY,
+    name varchar(255)
 );
 
 CREATE TABLE Route (
@@ -44,14 +51,11 @@ CREATE TABLE Payment_Method (
     name varchar(255)
 );
 
-CREATE TABLE Car (
-    id_car varchar(255) PRIMARY KEY,
-    id_user varchar(255),
-    id_brand varchar(255),
-    plate varchar(255),
-    color varchar(255),
-    model varchar(255),
-    FOREIGN KEY(id_brand) REFERENCES Brand (id_brand)
+CREATE TABLE Campus_UFSC (
+    id_campus varchar(255) PRIMARY KEY,
+    id_address varchar(255),
+    name varchar(255),
+    FOREIGN KEY(id_address) REFERENCES Address (id_address)
 );
 
 CREATE TABLE User (
@@ -61,19 +65,19 @@ CREATE TABLE User (
     age varchar(255),
     email varchar(255),
     cpf varchar(255),
-    password varchar(255)
+    password varchar(255),
+    FOREIGN KEY(id_campus) REFERENCES Campus_UFSC (id_campus)
 );
 
-CREATE TABLE Campus_UFSC (
-    id_campus varchar(255) PRIMARY KEY,
-    id_address varchar(255),
-    name varchar(255),
-    FOREIGN KEY(id_address) REFERENCES Address (id_address)
-);
-
-CREATE TABLE State (
-    id_state varchar(255) PRIMARY KEY,
-    name varchar(255)
+CREATE TABLE Car (
+    id_car varchar(255) PRIMARY KEY,
+    id_user varchar(255),
+    id_brand varchar(255),
+    plate varchar(255),
+    color varchar(255),
+    model varchar(255),
+    FOREIGN KEY(id_brand) REFERENCES Brand (id_brand),
+    FOREIGN KEY(id_user) REFERENCES User (id_user)
 );
 
 CREATE TABLE Travel_User (
@@ -109,23 +113,3 @@ CREATE TABLE Route_Address (
     FOREIGN KEY(id_route) REFERENCES Route (id_route),
     FOREIGN KEY(id_address) REFERENCES Address (id_address)
 );
-
-ALTER TABLE
-    POI
-ADD
-    FOREIGN KEY(id_address) REFERENCES Address (id_address);
-
-ALTER TABLE
-    City
-ADD
-    FOREIGN KEY(id_state) REFERENCES State (id_state);
-
-ALTER TABLE
-    Car
-ADD
-    FOREIGN KEY(id_user) REFERENCES User (id_user);
-
-ALTER TABLE
-    User
-ADD
-    FOREIGN KEY(id_campus) REFERENCES Campus_UFSC (id_campus);
