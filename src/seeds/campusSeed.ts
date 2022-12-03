8ce56e569048528ca8ae81904957d244153355cf
Matheus Jacks <matheusrjacks@gmail.com>
2022-12-03 20:10:29 -0300

    feat: add campus seed

---
+++ src/seeds/campusSeed.ts
@@ -0,0 +1,19 @@
+import { Connection } from "mysql2/promise";
+
+export default async (db: Connection) => {
+  // [id_campus, id_address, name]
+  const campi = [
+    ["1", "10", "Campus Araranguá"],
+    ["2", "11", "Campus Trindade"],
+    ["3", "12", "Campus Joinville"],
+    ["4", "13", "Campus Blumenau"],
+    ["5", "14", "Campus Curitibanos"],
+  ];
+
+  for (const data of campi) {
+    await db.execute(
+      `INSERT INTO ufscarona.campus_ufsc (id_campus, id_address, name) VALUES (?, ?, ?);`,
+      data
+    );
+  }
+};
