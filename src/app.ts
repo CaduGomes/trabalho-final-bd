d18e59afefb660ccf4f36e75b55bd21de283ce10
Cadu Gomes <carlos.gomes@konecty.com>
2022-12-03 20:51:16 -0300

    adicionando queries

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,16 @@
 import createConnection from "./database/createConnection";
 import generateDatabase from "./database/generateDatabase";
 import fakeData from "./database/inserts";
-import { searchTravel } from "./database/querys";
+import { getTravel, getTravels, getUserInfo } from "./database/queries";
 
 const app = async () => {
   const db = await createConnection();
 
   await generateDatabase(db);
 
-  await fakeData(db);
-
-  const result = await searchTravel(db, new Date("2021-06-01"), "3", "4");
-
-  console.table(result);
+  await getTravels(db, new Date("2021-06-01"), "3", "4");
+  await getTravel(db, "1");
+  await getUserInfo(db, "1");
 };
 
 export default app;
--- src/database/queries/index.ts
+++ src/database/queries/index.ts
@@ -1,6 +1,6 @@
 import { Connection } from "mysql2/promise";
 
-const searchTravel = async (
+const getTravels = async (
   db: Connection,
   date: Date,
   origin: string,
@@ -32,8 +32,65 @@ const searchTravel = async (
 
     console.table(result);
   } catch (err) {
+    console.log(`Error searching travels: ${(err as Error).toString()}`);
+  }
+};
+
+const getTravel = async (db: Connection, id: string) => {
+  try {
+    const sql = `
+    SELECT User.id_user, User.name, User.age, Campus_UFSC.name, Destination.cityName as destinationCityName, Destination.stateName as destinationStateName,
+    Origin.cityName as originCityName, Origin.stateName as originStateName from User
+	  INNER JOIN Campus_UFSC on Campus_UFSC.id_campus = User.id_campus
+	  INNER JOIN Travel on Travel.id_travel = ?
+    INNER JOIN
+	    (SELECT User.id_user, City.name as cityName, State.name as stateName from User
+	    INNER JOIN Travel_User on Travel_User.id_travel = ?
+	    INNER JOIN POI_Travel_User on POI_Travel_User.id_travel_user = Travel_User.id_travel_user
+	    INNER JOIN POI on POI.id_poi = POI_Travel_User.id_poi
+	    INNER JOIN Address on Address.id_address = POI.id_address
+	    INNER JOIN City on City.id_city = Address.id_city
+	    INNER JOIN State on State.id_state = City.id_state
+	    INNER JOIN Travel on Travel.id_travel = ?
+	    where POI_Travel_User.type = 'destination') Destination on Destination.id_user = User.id_user
+    INNER JOIN
+	    (SELECT User.id_user, City.name as cityName, State.name as stateName from User
+	    INNER JOIN Travel_User on Travel_User.id_travel = ?
+	    INNER JOIN POI_Travel_User on POI_Travel_User.id_travel_user = Travel_User.id_travel_user
+	    INNER JOIN POI on POI.id_poi = POI_Travel_User.id_poi
+	    INNER JOIN Address on Address.id_address = POI.id_address
+	    INNER JOIN City on City.id_city = Address.id_city
+	    INNER JOIN State on State.id_state = City.id_state
+	    INNER JOIN Travel on Travel.id_travel = ?
+	    where POI_Travel_User.type = 'origin') Origin on Origin.id_user = User.id_user;
+    `;
+
+    const result = await db.execute(sql, [id, id, id, id, id]);
+    console.table(result);
+  } catch (err) {
     console.log(`Error searching travel: ${(err as Error).toString()}`);
   }
 };
 
-export { searchTravel };
+const getUserInfo = async (db: Connection, id: string) => {
+  try {
+    const sql = `
+    SELECT 
+    User.name, User.age, User.email, User.password, User.cpf, 
+    Campus_UFSC.name, 
+    Car.model, Car.color, Car.plate,
+    Brand.name
+    from User
+    INNER JOIN Campus_UFSC on User.id_campus = Campus_UFSC.id_campus
+    INNER JOIN Car on User.id_user = Car.id_user
+    INNER JOIN Brand on Car.id_brand = Brand.id_brand
+    where User.id_user = ?;
+    `;
+    const result = await db.execute(sql, [id]);
+    console.table(result);
+  } catch (err) {
+    console.log(`Error searching user: ${(err as Error).toString()}`);
+  }
+};
+
+export { getTravels, getTravel, getUserInfo };
