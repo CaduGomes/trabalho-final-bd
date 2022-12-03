b9d3cfecd134a1556f9929216b19be92c4fa08cf
Matheus Jacks <matheusrjacks@gmail.com>
2022-12-03 20:45:12 -0300

    feat: add poi travel user seed

---
+++ src/seeds/POITravelUser.ts
@@ -0,0 +1,17 @@
+import { Connection } from "mysql2/promise";
+
+export default async (db: Connection) => {
+  // [id_travel_user, id_poi, type, order_number]
+  const POITravelUsers = [
+    ["1", "1", "origin", "1"],
+    ["1", "3", "between", "2"],
+    ["1", "4", "destination", "3"]
+  ];
+
+  for (const data of POITravelUsers) {
+    await db.execute(
+      `INSERT INTO ufscarona.travel_user (id_travel_user, id_poi, type, order_number) VALUES (?, ?, ?, ?, ?);`,
+      data
+    );
+  }
+};
