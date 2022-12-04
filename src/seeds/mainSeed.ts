b8ade500f7d230a71bf15f8999c34320c4f2a954
Matheus Jacks <matheusrjacks@gmail.com>
2022-12-03 21:06:26 -0300

    feat: update mainSeed

--- src/seeds/mainSeed.ts
+++ src/seeds/mainSeed.ts
@@ -1,6 +1,23 @@
 import { Connection } from "mysql2/promise";
 import addressSeed from "./addressSeed";
+import campusSeed from "./campusSeed";
+import paymentMethodSeed from "./paymentMethodSeed";
+import paymentMethodTravelSeed from "./paymentMethodTravelSeed";
+import POISeed from "./POISeed";
+import POITravelUser from "./POITravelUser";
+import travelSeed from "./travelSeed";
+import travelUserSeed from "./travelUserSeed";
+import userSeed from "./userSeed";
 
 export default async (db: Connection) => {
   await addressSeed(db);
+  await campusSeed(db);
+  await POISeed(db);
+  await userSeed(db);
+  await paymentMethodSeed(db);
+  await travelSeed(db);
+  await travelUserSeed(db);
+
+  await paymentMethodTravelSeed(db);
+  await POITravelUser(db);
 };
