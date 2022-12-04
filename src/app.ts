5524a23f1a046b557530e4e32ab3d46969fd21c0
Matheus Jacks <matheusrjacks@gmail.com>
2022-12-03 21:04:58 -0300

    refactor: delete unused code

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,11 @@
 import createConnection from "./database/createConnection";
 import generateDatabase from "./database/generateDatabase";
-import fakeData from "./database/inserts";
 import { searchTravel } from "./database/queries";
 
 const app = async () => {
   const db = await createConnection();
 
   await generateDatabase(db);
-
-  await fakeData(db);
 
   const result = await searchTravel(db, new Date("2021-06-01"), "3", "4");
 
