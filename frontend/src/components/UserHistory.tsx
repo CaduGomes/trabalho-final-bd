32b9ac344a2b7aa698fedf67c505bf457ebcf4a9
Cadu Gomes <carlos.gomes@konecty.com>
2022-12-11 21:31:30 -0300

    corrigindo front-end

--- frontend/src/components/UserHistory.tsx
+++ frontend/src/components/UserHistory.tsx
@@ -3,7 +3,18 @@ import { useUser } from "../context/auth";
 
 const UserHistory: React.FC = () => {
   const { user } = useUser();
-  const [travels, setTravels] = useState([]);
+  const [travels, setTravels] = useState<
+    {
+      travelId: string;
+      travelDate: string;
+      travelPrice: string;
+      poiOriginName: string;
+      poiOriginId: string;
+      poiDestinationName: string;
+      poiDestinationId: string;
+    }[]
+  >([]);
+  console.log(travels);
 
   const authHandler = async () => {
     if (user.name === "") {
@@ -11,9 +22,11 @@ const UserHistory: React.FC = () => {
       return;
     }
 
+    console.log(user);
+
     try {
       const response = await fetch(
-        `http://localhost:3001/travel/user?id=${user.id}`
+        `http://localhost:3001/user/${user.id_user}/travels`
       );
 
       if (response.status === 200) {
@@ -36,6 +49,38 @@ const UserHistory: React.FC = () => {
         >
           Buscar suas caronas
         </button>
+
+        <div className="flex flex-col gap-2">
+          {travels.map((travel) => (
+            <div className="flex flex-col gap-2 border border-slate-600 rounded-md p-2">
+              <div>
+                <span className="font-bold">Data: </span>
+                <span>{travel.travelDate}</span>
+              </div>
+              <div>
+                <span className="font-bold">Preço: </span>
+                <span>{travel.travelPrice}</span>
+              </div>
+              <div>
+                <span className="font-bold">Origem: </span>
+                <span>{travel.poiOriginName}</span>
+              </div>
+              <div>
+                <span className="font-bold">Destino: </span>
+                <span>{travel.poiDestinationName}</span>
+              </div>
+              <div>
+                <span className="font-bold">ID da carona: </span>
+                <span>{travel.travelId}</span>
+              </div>
+              <div>
+                <span className="font-bold">Preço: </span>
+
+                <span>{travel.travelPrice}</span>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
